70ed57c5054802aa06bb1200dc01eb7c21bcd2d0
Vladislav Shchapov <vladislav@shchapov.ru>
2022-06-04 19:50:28 +0500

    test2

---
+++ test/std-filesystem-test.cc
@@ -0,0 +1,22 @@
+// Formatting library for C++ - tests of formatters for standard library types
+//
+// Copyright (c) 2012 - present, Victor Zverovich
+// All rights reserved.
+//
+// For the license information refer to format.h.
+
+#include "fmt/std.h"
+#include "gtest/gtest.h"
+
+TEST(std_filesystem_test, path) {
+#ifdef __cpp_lib_filesystem
+  EXPECT_EQ(fmt::format("{:8}", std::filesystem::path("foo")), "\"foo\"   ");
+  EXPECT_EQ(fmt::format("{}", std::filesystem::path("foo\"bar.txt")),
+            "\"foo\\\"bar.txt\"");
+
+#  ifdef _WIN32
+  EXPECT_EQ(fmt::format("{}", std::filesystem::path(L"Файл.txt")),
+            "\"\xd0\xa4\xd0\xb0\xd0\xb9\xd0\xbb.txt\"");
+#  endif
+#endif
+}
